33822f5ce4950eab65931ec83a5f5be1827b5c77
whoorayoung <91642185+whoorayoung@users.noreply.github.com>
2022-01-25 19:01:01 -0800

    Create Tic-Tac-Toe-game.py

---
+++ Tic-Tac-Toe-game.py
@@ -0,0 +1,23 @@
+def main():
+    pass
+
+def create_board():
+    pass
+
+def display_board():
+    pass
+
+def x_or_o():
+    pass
+
+def winner():
+    pass
+
+def make_move():
+    pass
+
+def player():
+    pass
+
+if __name__ == "__main__":
+    main()
